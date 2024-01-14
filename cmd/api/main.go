ff467ab13ff1901d5e202f21c0855de94b266753
Code Mountains <thecodemountains@gmail.com>
2024-01-14 15:23:41 -0600

    handlers.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
+const version = "0.0.1"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
 func main() {
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 8000, "API Server Port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|prod)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	addr := fmt.Sprintf(":%d", cfg.port)
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", healthcheck)
-	err := http.ListenAndServe(":8000", mux)
+	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
+
+	logger.Printf("starting %s server on %s", cfg.env, addr)
+
+	err := http.ListenAndServe(addr, mux)
 
 	if err != nil {
 		fmt.Println(err)
@@ -23,6 +57,6 @@ func healthcheck(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "env: %s\n", "dev")
-	fmt.Fprintf(w, "version: %s\n", "0.0.1")
+	fmt.Fprintf(w, "env: %s\n", cfg.env)
+	fmt.Fprintf(w, "version: %s\n", version)
 }
